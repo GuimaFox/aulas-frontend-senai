fdaf5c574396844d8f7aa0ff1361603388f437ff
ANDRÉ LUIZ DE ASSIS GUIMARÃES <andre_l_guimaraes@estudante.sesisenai.org.br>
2024-03-27 22:30:49 -0300

    Adicionar Formulário e botão calculadora

--- src/componentes/Principal/Principal.jsx
+++ src/componentes/Principal/Principal.jsx
@@ -1,12 +1,34 @@
-
+import React from 'react';
+import BotaoContador from '../BotaoContador/BotaoContador';
+import Formulario from '../Fomulario/Formulario';
 import BotaoCustomizado from '../BotaoCustomizado/BotaoCustomizado';
 import ListaProdutos from '../ListaProdutos/ListaProdutos';
 import './Principal.css';
 
 const Principal = () => {
+const[exemplo, setExemplo] = React.useState('');
+
   return (
   <main>
-    <BotaoCustomizado
+    <nav>
+
+    <BotaoCustomizado tipo='primario' onClick = {()=> setExemplo('listaProdutos')}>
+      ListaProdutos
+    </BotaoCustomizado>
+    <BotaoCustomizado tipo='secundario' onClick = {()=> setExemplo('botaoContador')}>
+      BotaoContador
+    </BotaoCustomizado>
+    <BotaoCustomizado tipo='' onClick = {()=> setExemplo('formulario')}>
+      Formulário
+    </BotaoCustomizado>
+
+    </nav>
+
+    {exemplo === 'listaProdutos' && <ListaProdutos />}
+    {exemplo === 'botaoContador' && <BotaoContador />}
+    {exemplo === 'formulario' && <Formulario />}
+    
+    {/*<BotaoCustomizado
      tipo='primario'
       onClick={() => { 
       alert('Salvo com sucesso');
@@ -26,8 +48,9 @@ const Principal = () => {
       alert('Cancelou tudo!!!');
     }}
     >Cancelar</BotaoCustomizado>
+    <BotaoContador />
+    <ListaProdutos />*/}
 
-    <ListaProdutos />
 
   </main>
   );
